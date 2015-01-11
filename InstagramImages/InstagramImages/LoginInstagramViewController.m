a7c5df9c153d6888ff4a80cfecd6c86b868faae9
Daniel Gonzalez <danman2828@Daniel-Gonzalezs-MacBook-Pro.local>
2015-01-11 17:48:00 -0600

    finished

--- InstagramImages/InstagramImages/LoginInstagramViewController.m
+++ InstagramImages/InstagramImages/LoginInstagramViewController.m
@@ -10,14 +10,15 @@
 #import "InstagramConstants.h"
 #import "NSDictionary+UrlEncoding.h"
 #import "WebServiceHandler.h"
+#import "ImageDownloaded.h"
 
-@interface LoginInstagramViewController () <jsonDonwloadHandler>
-@property (weak, nonatomic) IBOutlet UIScrollView *mainScrollView;
+@interface LoginInstagramViewController () <jsonDonwloadHandler, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout>
+@property (weak, nonatomic) IBOutlet UICollectionView *mainCollectionView;
 @property (strong, nonatomic) NSArray* imagesSource;
+@property (nonatomic, assign) NSInteger selectedCellIndex;
 @end
 
 __weak LoginInstagramViewController* weakSelf;
-//NSMutableArray* sortedImages;
 
 @implementation LoginInstagramViewController
 
@@ -27,7 +28,8 @@ __weak LoginInstagramViewController* weakSelf;
 -(void)setImagesSource:(NSArray *)imagesSource{
     dispatch_async(dispatch_get_main_queue(), ^{
         _imagesSource = imagesSource;
-#warning reload data here
+        [self setSelectedCellIndex:-1];//no cell is selected at the moment
+        [[self mainCollectionView] reloadData];
     });
 }
 
@@ -36,9 +38,7 @@ __weak LoginInstagramViewController* weakSelf;
 - (void)viewDidLoad {
     [super viewDidLoad];
     weakSelf = self;
-    //sortedImages = [NSMutableArray new];
     [[WebServiceHandler sharedInstance] setDelegateVC:self];
-//    [[WebServiceHandler sharedInstance] sessionUsingUrl:[NSURL URLWithString:[NSString stringWithFormat:@"%@/v1/media/popular?client_id=%@",INSTAGRAM_DOMAIN,INSTAGRAM_CLIENT_ID]]];
     [[WebServiceHandler sharedInstance] sessionUsingUrl:[NSURL URLWithString:[NSString stringWithFormat:@"%@/v1/tags/%@/media/recent?client_id=%@",INSTAGRAM_DOMAIN,INSTAGRAM_TAG,INSTAGRAM_CLIENT_ID]]];
 }
 
@@ -61,23 +61,6 @@ __weak LoginInstagramViewController* weakSelf;
     return NO;
 }
 
-//-(void)loadAndDisplayImages:(NSArray*)images{
-//    float screenWidth = [[self mainScrollView] frame].size.width;
-//    float spacer = 100;
-//    float currentY = spacer;
-//    for (NSDictionary* image in images) {
-//        float width = [image[@"width"] integerValue]*0.5;
-//        float height = [image[@"height"] integerValue]*0.5;
-//        UIImageView* view = [[UIImageView alloc] initWithFrame:CGRectMake((screenWidth*0.5)-(width*0.5), currentY, width, height)];
-//        [view setUserInteractionEnabled:YES];
-//        [view setImage:[UIImage imageNamed:@"loading.jpg"]];
-//        [[self mainScrollView] addSubview:view];
-//        currentY+=height+spacer;
-//    }
-//    
-//#warning change the scrollview size
-//}
-
 #pragma mark - webServiceHandler delegate
 
 -(void)didFinishDownloadingJson:(NSDictionary*)json{
@@ -89,15 +72,11 @@ __weak LoginInstagramViewController* weakSelf;
     NSMutableArray* dictImages = [NSMutableArray new];
     int orderSize = 0; //counter to order images by big,small,small (repeat)
     for (NSDictionary* user in json[@"data"]) {
-//        NSArray* tags = user[@"tags"];
-//        if (![weakSelf array:tags containsString:INSTAGRAM_TAG]) {
-//            continue;
-//        }
         NSDictionary* images = [user objectForKey:@"images"];
         if (orderSize%3==0) { //if 0 use big image
-            [dictImages addObject:[images objectForKey:@"standard_resolution"]];
+            [dictImages addObject:[NSMutableDictionary dictionaryWithDictionary:[images objectForKey:@"standard_resolution"]]];
         }else{ //use small image
-            [dictImages addObject:[images objectForKey:@"low_resolution"]];
+            [dictImages addObject:[NSMutableDictionary dictionaryWithDictionary:[images objectForKey:@"low_resolution"]]];
         }
         orderSize++;
     }
@@ -108,5 +87,52 @@ __weak LoginInstagramViewController* weakSelf;
     [self showAlertMessage:error];
 }
 
+#pragma mark - Collection View
+#pragma mark DataSource delegate
+
+-(NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView{
+    return 1;
+}
+
+-(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
+    return [[self imagesSource] count];
+}
+
+-(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
+    UICollectionViewCell* cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"cellImage" forIndexPath:indexPath];
+    __weak NSMutableDictionary* dictImage= [[self imagesSource] objectAtIndex:[indexPath row]%[[self imagesSource] count]];
+    //we use tags so we can retrive the actual imageView object and we don't need a custom class cell with the UIImageView IBOutlet property
+    ImageDownloaded* imageView = (ImageDownloaded*)[cell viewWithTag:28];
+    [imageView setImageFromDictionary:dictImage];
+    return cell;
+}
+
+#pragma mark FlowLayout delegate
+
+-(CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath{
+    //we ensure the array doesn't get out of bounds with "%" so if infinite scroll is implemented there's always content to display
+    NSInteger index = [indexPath row];
+    NSMutableDictionary* dictImage = [[self imagesSource] objectAtIndex:index%[[self imagesSource] count]];
+    CGFloat minSize = 50;
+    CGFloat factor = index==[self selectedCellIndex]?0.5:0.25; //using 0.5 and 0.25 because images are too big
+    return CGSizeMake(MAX([[dictImage objectForKey:@"width"] integerValue]*factor,minSize), MAX([[dictImage objectForKey:@"height"] integerValue]*factor,minSize));
+}
+
+-(UIEdgeInsets)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout insetForSectionAtIndex:(NSInteger)section{
+    //20 is a sutable margin
+    return UIEdgeInsetsMake(20, 20, 20, 20);
+}
+
+#pragma  mark collectionView delegate
+
+-(void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath{
+    if ([self selectedCellIndex]==[indexPath row]) {
+        //if it is already selected, return it to its size
+        [self setSelectedCellIndex:-1];
+    }else{
+        [self setSelectedCellIndex:[indexPath row]];
+    }
+    [[[self mainCollectionView] collectionViewLayout] invalidateLayout];
+}
 
 @end
---
+++ InstagramImages/InstagramImages/imageDownloaded.h
@@ -0,0 +1,15 @@
+//
+//  ImageDownloaded.h
+//  InstagramImages
+//
+//  Created by Daniel Gonzalez on 1/11/15.
+//  Copyright (c) 2015 Daniel Gonzalez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImageDownloaded : UIImageView
+
+-(void)setImageFromDictionary:(__weak NSMutableDictionary*)imageDict;
+
+@end
